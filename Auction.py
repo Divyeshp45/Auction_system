f58edea785b4ce9e57035a6eeb40a7e9e654315b
Divyesh <divyeshcoder452@gmail.com>
2022-01-25 20:31:42 +0530

    made changes in highest_bid function

--- Auction.py
+++ Auction.py
@@ -16,21 +16,24 @@ clear()
 
 others=input("if there are more people to bid enter  'YES'or 'NO' : \n").lower()
 while others=="yes":
-    if others=="yes":
+    
         name=input("ENTER YOUR NAME: \n")
         bid=input("ENTER YOUR BID :  \n")
         name_list[name]=bid
         clear()
-    others=input("if there are more people to bid enter  'YES'or 'NO' : \n").lower()
-#function to find highest bid
+        others=input("if there are more people to bid enter  'YES'or 'NO' : \n").lower()
+#function to find highest bid 
 def highest_bid():      
     highest_bid=0  
     for key in name_list:
         bid_amount=int(name_list[key] ) 
         if bid_amount>highest_bid:
           highest_bid=bid_amount
-    winner_name=key  
+          winner_name=key  
     print(f"{winner_name} is Winner with bid {highest_bid}/- ")
 
-highest_bid()  
+highest_bid()
+
+
+
     
